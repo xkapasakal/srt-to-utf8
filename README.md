e35d6556b6f40cb1c129eda501b069d55b5e7127
Christos Kapasakalidis <xkapasakal@gmail.com>
2013-12-23 02:42:38 +0200

    Accept single file as input

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ srt-to-utf8
 ===========
 
 Transform srt files to utf-8 encoding
+
+```gem build srt-to-utf8.gemspec```
+```gem install ./srt-to-utf8-0.0.1.gem```
--- lib/srt-to-utf8.rb
+++ lib/srt-to-utf8.rb
@@ -1,20 +1,29 @@
 #!/usr/bin/env ruby
 
 require 'rchardet19'
+require 'term/ansicolor'
 
-Dir.chdir("#{ARGV[0]}")
-Dir.glob('**/*.srt').each do|file_name|
-	file = File.open(file_name, "rb")
-	contents = file.read
-	char_detection = CharDet.detect(contents)
+class String
+  include Term::ANSIColor
+end
+
+class Converter
+
+  def convert(file_name)
+    file = File.open(file_name, 'rb')
+    contents = file.read
+    char_detection = CharDet.detect(contents)
     encoding = char_detection['encoding']
     confidence = char_detection['confidence'] # 0.0 <= confidence <= 1.0
-    if encoding.start_with?("ISO-8859-") and confidence > 0.9
-    	contents.force_encoding(encoding)
-    	contents_utf8 = contents.encode("UTF-8")
-    	f = File.new(file_name, "w")
-		f.write(contents_utf8)
-		f.close                
-		puts "Transformed #{file_name} from encoding #{encoding} to utf-8 with confidence #{confidence}"
+    if !(encoding == 'utf-8') and confidence > 0.9
+      contents.force_encoding(encoding)
+      contents_utf8 = contents.encode('UTF-8')
+      f = File.new(file_name, 'w')
+      f.write(contents_utf8)
+      f.close
+      puts "Transformed #{file_name} from encoding #{encoding} to utf-8 with confidence #{confidence}".green
+    else
+      puts "Didn't transformed #{file_name} with encoding #{encoding} and confidence #{confidence}".red
     end
+  end
 end
